f0889b8ac1dfbd7ff1cc0f982726858310fc19d2
Marya <maryammuchai@gmail.com>
2021-03-26 22:05:09 +0300

    Styling the inital page html

--- Documents/moringa-core/Quote-App/Quotes/src/app/app.component.ts
+++ Documents/moringa-core/Quote-App/Quotes/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Quote {
+  quote: string;
+  author: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  loading: boolean = true;
+  
   title = 'Quotes';
 }
