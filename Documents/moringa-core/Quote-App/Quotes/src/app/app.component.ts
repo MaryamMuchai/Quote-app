ee4b723782091ee292fb75d2e911592a355596f1
Marya <maryammuchai@gmail.com>
2021-03-27 22:48:52 +0300

    adding the user option section

--- Documents/moringa-core/Quote-App/Quotes/src/app/app.component.ts
+++ Documents/moringa-core/Quote-App/Quotes/src/app/app.component.ts
@@ -1,18 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-interface Quote {
-  quote: string;
-  author: string;
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit {
-  loading: boolean = true;
-  
-  title = 'Quotes';
+export class AppComponent {
+  quotes:string[];
+
+constructor() {
+  this.quotes = ['Full name', 'Your Quote']
+}
 }
