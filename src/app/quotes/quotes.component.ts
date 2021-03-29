4a8737614cc69049575e9796bccc62a246de87db
Marya <maryammuchai@gmail.com>
2021-03-30 00:59:47 +0300

    resolving date issue

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Quote } from '../quote';
 
 @Component({
@@ -9,20 +9,19 @@ import { Quote } from '../quote';
 export class QuotesComponent implements OnInit {
   title = 'Welcome to the Quote App';
   quotes:Quote[] = [
-    new Quote (1,'The purpose of our lives is to be happy.','Dalai Lama.' ,new Date(2021,1,1),1,0),
-    new Quote (2,'Life is what happens when you are busy making other plans.','John Lennon',new Date(2021,3,3),0,0),
-    new Quote (3,'Get busy living or get busy dying.', 'Stephen King', new Date(2021,2,2),0,0),
+    new Quote (1,'The purpose of our lives is to be happy.','Dalai Lama.',new Date(2021, 1, 1),0,0),
+    new Quote (2,'Life is what happens when you are busy making other plans.','John Lennon',new Date(2021, 3, 3),0,0),
+    new Quote (3,'Get busy living or get busy dying.', 'Stephen King', new Date(2021, 2, 2),0,0),
   ];
   get sortQuotes() {
-    return this.quotes.sort((a, b) => {
-      return <any>new Date(b.datePosted) - <any>new Date(a.datePosted);
-    });
+    return this.quotes;
+  
   }
   addedQuote(quote: Quote){
     let arraysize = this.quotes.length;
     quote.id = arraysize+1;
     Quote.completeDate = new Date(Quote.completeDate)
-    this.quotes.push(quote)
+    this.quotes.push(quote);
 
   }
   quoteDelete(isRead: any, index: number){
