4df711f6ea0581d26a0b2093e390d6294dd6e33a
Maryam <78798386+MaryamMuchai@users.noreply.github.com>
2021-03-29 22:14:20 +0300

    Delete Documents/moringa-core/Quote-App/Quotes directory

--- Documents/moringa-core/Quote-App/Quotes/src/app/through.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ThroughDirective } from './through.directive';
-
-describe('ThroughDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ThroughDirective();
-    expect(directive).toBeTruthy();
-  });
-});
